777ca80111fdb7674c9371fdb5c22cf88daf0f20
Hiroaki0117 <nishimura.hn.0117@gmail.com>
2023-03-17 14:55:33 +0900

    07 add_header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,10 +73,7 @@ Rails.application.configure do
   
   Rails.application.configure do
     # default url
-    config.action_mailer.default_url_options = {
-      protocol: 'https',
-      host: "http://localhost:3000/"
-    }
+    config.action_mailer.default_url_options = { host:'localhost:3000' }
     # mail setting
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
